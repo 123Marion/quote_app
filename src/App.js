066bdc346ee6cb9a3db08432456d1036361557ee
123Marion <ngochocharity@gmail.com>
2022-09-15 18:29:37 +0300

    adds constance for user , comments and quotes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // import logo from './logo.svg';
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import "./App.css";
 import Hero from "./components/home/Hero";
 import AddQuote from "./components/quote/AddQuote";
+import ViewQuote from "./components/quote/ViewQuote";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [comments, setComments] = useState([]);
+  const [quotes, setQuotes] = useState([]);
+  const [user, setUser] = useState([]);
+
+  function handleAddComments(newComments) {
+    setComments([...comments, newComments]);
+  }
+
   return (
     <div className="App">
       {/* {!user ? (
@@ -16,6 +26,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Hero />} />
           <Route exact path="/add-quote" element={<AddQuote />} />
+          <Route
+            exact
+            path="/add-comment/:id"
+            element={<ViewQuote onAddComment={handleAddComments} />}
+          />
         </Routes>
       </>
       {/* )} */}
