13d2d9d6b45ee068834b1840c094ed01eabcd090
123Marion <ngochocharity@gmail.com>
2022-09-15 12:21:22 +0300

    creates hero page adds quotes component and quotes

---
+++ src/components/home/Hero.js
@@ -0,0 +1,27 @@
+import HeroCSS from "./Hero.module.css";
+
+import React from "react";
+import Quotes from "../quote/Quotes";
+
+function Hero() {
+  return (
+    <>
+        <header className={HeroCSS.section + " " + HeroCSS.hero}>
+          <div className="columns">
+            <div className="column is-12">
+              <div className={HeroCSS.image}>
+                <img
+                  alt="featured quote of the day"
+                  src="https://images.unsplash.com/photo-1552508744-1696d4464960?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                ></img>
+              </div>
+            </div>
+          </div>
+        </header>
+      <section className={HeroCSS.section}>
+        <Quotes />
+      </section>
+    </>
+  );
+}
+export default Hero;
