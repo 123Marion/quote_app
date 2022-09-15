7bfb1136e8a13b16360e0b322c08a9a13ddf5fb7
123Marion <ngochocharity@gmail.com>
2022-09-15 18:33:16 +0300

    creates comment appearance with a border and user

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,6 +1,18 @@
 import React from "react";
-
-function Comment(){
-    return <></>
+import CommentCSS from "./Comment.module.css";
+function Comment() {
+  return (
+    <>
+      <div className={CommentCSS.comment}>
+        <div>
+            <span>username</span>
+            <span>date posted</span>
+        </div>
+        <div className={CommentCSS.content}>
+            content
+        </div>
+      </div>
+    </>
+  );
 }
 export default Comment;
