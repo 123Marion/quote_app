cf971e410fd0587eef73c426214704b2d0362f96
123Marion <ngochocharity@gmail.com>
2022-09-15 23:20:11 +0300

    a view for each quote

--- src/components/quote/Quotes.js
+++ src/components/quote/Quotes.js
@@ -3,30 +3,34 @@ import React from "react";
 // import image from "../../logo.svg";
 import { Link } from "react-router-dom";
 
-function Card() {
+function Quotes({ quotes }) {
   return (
     <>
-      <div className={QuoteCSS.fullCard}>
-        <div className="columns is-multiline">
-          <div className="column is-12">
-            <div className={QuoteCSS.quote}>
-              <div>
-                <p>hello there i am here</p>
+      {quotes.map((quote) => (
+        <div className={QuoteCSS.fullCard} key={quote.id}>
+          <div className="columns is-multiline">
+            <div className="column is-12">
+              <div className={QuoteCSS.quote}>
                 <div>
-                  <Link to={`/add-comment/1`}>
-                    <div>comment</div>
-                  </Link>
+                  <p>{quote.content}</p>
+                  <div>
+                    <Link to={`/add-comment/1`}>
+                      <div>comment</div>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <div>Author</div>
-                <div>date</div>
+                <div>
+                  <div>{quote.author}</div>
+                  <div>
+                    <em>{new Date(quote.created_at).toLocaleDateString()}</em>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
-export default Card;
+export default Quotes;
